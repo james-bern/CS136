51ce8a676f22ef06ac0416b788d8ae1c24868fdc
Jim <jmb15@williams.edu>
2023-05-15 22:33:55 -0400

    asdf

--- App/BuildAndRun.bat
+++ App/BuildAndRun.bat
@@ -1,1 +1,1 @@
-((clear && javac *.java && java $basename $1 && rm *.class) || (cls && javac *.java && java %~n1 && del *.class)) 
+((clear && javac *.java && java $basename -s .java $1 && rm *.class) || (cls && javac *.java && java %~n1 && del *.class)) 
