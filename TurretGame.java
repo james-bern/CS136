27f7ec8b3283ac8d868d7271bbebaf0aa98077eb
Jim <jmb15@williams.edu>
2023-08-11 20:08:13 -0400

    poosh

--- TurretGame.java
+++ TurretGame.java
@@ -1,69 +1,85 @@
 class Player {
     Vector2 position;
-    static final double RADIUS = 2.0;
+
+    static final double radius = 2.0;
 }
 
 class Bullet {
     Vector2 position;
     Vector2 velocity;
     boolean alive;
-    static final double RADIUS = 1.0;
+    int age;
+
+    static final double radius = 1.0;
 }
 
 class Turret {
     Vector2 position;
     int framesSinceFired;
-    static final Vector2 SIZE = new Vector2(8.0, 8.0);
+
+    static final double radius = 8.0;
 }
 
 class TurretGame extends App {
+
     Player player;
+
     Turret turret;
 
+    // bullets is a big, fixed-size array of Bullet objects (slots)
+    // initially all bullets are "dead," i.e. not updated or drawn
+    // think of dead bullets as empty slots in the bullets array
     Bullet[] bullets;
-    int indexOfNextBullet;
 
     void setup() {
-        bullets = new Bullet[256];
-        for (int bulletIndex = 0; bulletIndex < bullets.length; ++bulletIndex) {
-            bullets[bulletIndex] = new Bullet();
-        }
-        indexOfNextBullet = 0;
+        player = new Player();
+        player.position = new Vector2(0.0, -32.0);
 
         turret = new Turret();
         turret.position = new Vector2(0.0, 0.0);
 
-        player = new Player();
-        player.position = new Vector2(0.0, -32.0);
+        bullets = new Bullet[256];
+        for (int bulletIndex = 0; bulletIndex < bullets.length; ++bulletIndex) {
+            bullets[bulletIndex] = new Bullet();
+        }
     }
 
     void loop() {
+        // player
         if (keyHeld('W')) { player.position.y += 1.0; }
         if (keyHeld('A')) { player.position.x -= 1.0; }
         if (keyHeld('S')) { player.position.y -= 1.0; }
         if (keyHeld('D')) { player.position.x += 1.0; }
+        drawCircle(player.position, player.radius, Vector3.cyan);
 
-        drawCircle(player.position, player.RADIUS, Vector3.red);
-
-
+        // turret
         if (turret.framesSinceFired++ == 16) {
             turret.framesSinceFired = 0;
 
-            Bullet bullet = bullets[indexOfNextBullet];
-            indexOfNextBullet = (indexOfNextBullet + 1) % bullets.length;
-
-            bullet.position = turret.position;
-            bullet.velocity = Vector2.directionFrom(turret.position, player.position);
-            bullet.alive = true;
+            // create bullet (in first empty slot)
+            for (int bulletIndex = 0; bulletIndex < bullets.length; ++bulletIndex) {
+                Bullet bullet = bullets[bulletIndex];
+                if (!bullet.alive) {
+                    bullet.alive = true;
+                    bullet.age = 0;
+                    bullet.position = turret.position;
+                    bullet.velocity = Vector2.directionFrom(turret.position, player.position);
+                    break;
+                }
+            }
         }
+        drawCircle(turret.position, turret.radius, Vector3.red);
 
-        drawCenterRectangle(turret.position, turret.SIZE, Vector3.cyan);
-
-
-        for (Bullet bullet : bullets) {
+        // bullets
+        for (int bulletIndex = 0; bulletIndex < bullets.length; ++bulletIndex) {
+            Bullet bullet = bullets[bulletIndex];
             if (!bullet.alive) { continue; }
+            if (bullet.age++ > 128) {
+                bullet.alive = false;
+                continue;
+            }
             bullet.position = bullet.position.plus(bullet.velocity);
-            drawCircle(bullet.position, bullet.RADIUS, Vector3.yellow);
+            drawCircle(bullet.position, bullet.radius, Vector3.red);
         }
     }
 
