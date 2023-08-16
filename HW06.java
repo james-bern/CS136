cf9b477bcae244a64d4af5c13309f5095d3b082d
Jim <jmb15@williams.edu>
2023-08-16 11:27:39 -0400

    poosh

--- HW06.java
+++ HW06.java
@@ -8,9 +8,11 @@ class Main {
         private double  valueIfTypeDouble;
         private String  valueIfTypeString;
         
-        static final int TYPE_BOOLEAN = 0;
-        static final int TYPE_DOUBLE  = 1;
-        static final int TYPE_STRING  = 2;   
+        static final int TYPE_BOOLEAN    = 0;
+        static final int TYPE_DOUBLE     = 1;
+        static final int TYPE_STRING     = 2;
+        static final String[] _typeNames = { "boolean", "double", "String" };
+        static String stringFromType(int type) { return _typeNames[type]; }
         
         Token(boolean value) { type = TYPE_BOOLEAN; valueIfTypeBoolean = value; } 
         Token(double  value) { type = TYPE_DOUBLE;  valueIfTypeDouble  = value; }
@@ -18,15 +20,15 @@ class Main {
         
         public String toString() {
             if (type == TYPE_BOOLEAN) { return "" + valueIfTypeBoolean; }
-            if (type == TYPE_DOUBLE ) { return " " + valueIfTypeDouble;  }
-            if (type == TYPE_STRING ) { return " " + valueIfTypeString;  }
-            assert false;
+            if (type == TYPE_DOUBLE ) { return "" + valueIfTypeDouble;  }
+            if (type == TYPE_STRING ) { return "" + valueIfTypeString;  }
+            assert false : "Token type not recognized; Token type = " + type;
             return "";
         }
         
-        boolean getBoolean() { assert type == TYPE_BOOLEAN; return valueIfTypeBoolean; }
-        double  getDouble()  { assert type == TYPE_DOUBLE;  return valueIfTypeDouble;  }
-        String  getString()  { assert type == TYPE_STRING;  return valueIfTypeString;  }
+        boolean getBoolean() { assert type == TYPE_BOOLEAN : "Token type is " + stringFromType(type); return valueIfTypeBoolean; }
+        double  getDouble()  { assert type == TYPE_DOUBLE  : "Token type is " + stringFromType(type); return valueIfTypeDouble;  }
+        String  getString()  { assert type == TYPE_STRING  : "Token type is " + stringFromType(type); return valueIfTypeString;  }
         
         static ArrayList<Token> tokenize(String postScriptProgram) {
             ArrayList<Token> result = new ArrayList<>();
@@ -47,15 +49,22 @@ class Main {
         }
     }
     
-    static class Interpreter {    
-        Stack<Token> _stack; // NOTE: do not access _stack directly
-                             //       instead, use stackPop*(), stackPush(...), and stackPrint()
+    static class Interpreter {
+        // NOTE: do not access _stack directly (but you can inspect it in the debugger!)
+        //       instead, use stackPop*(), stackPush(...), and stackPrint()
+        Stack<Token> _stack; 
+
+        // NOTE: do not access _symbolTable directly (but you can inspect it in the debugger!)
+        // NOTE: instead, use symbolTablePut() and symbolTableGet()
+        HashMap<String, Token> _symbolTable;
+        
         
         Interpreter() {
             _stack = new Stack<>();
+            _symbolTable = new HashMap<>();
         }
         
-        Token   stackPopToken()   { assert _stack.size() > 0; return _stack.pop(); }
+        Token   stackPopToken()   { assert _stack.size() > 0 : "can't pop; stack is empty"; return _stack.pop(); }
         boolean stackPopBoolean() { Token token = stackPopToken(); return token.getBoolean(); }
         double  stackPopDouble()  { Token token = stackPopToken(); return token.getDouble();  }
         String  stackPopString()  { Token token = stackPopToken(); return token.getString();  }
@@ -71,6 +80,14 @@ class Main {
             for (int i = tmp.size() - 1; i >= 0; --i) { System.out.println(tmp.get(i)); }
             System.out.println("-----");
             System.out.println("stack");
+        }
+        
+        void symbolTablePut(String key, Token value) {
+            // TODO: assert?
+        }
+        
+        Token symbolTableGet(String key) {
+            // TODO: assert
         }
         
         void interpret(ArrayList<Token> tokens, boolean PRINT_DEBUG_INFO) {
@@ -101,7 +118,7 @@ class Main {
     
     public static void main(String[] arguments) {
         Interpreter interpreter = new Interpreter();
-        ArrayList<Token> tokens = Token.tokenize("4.0 2.0 add 1.0 sub 5.0 eq");
-        interpreter.interpret(tokens, true);
+        ArrayList<Token> tokens = Token.tokenize("4.0 2.0 add 1.0 sub 5.0 eq pstack");
+        interpreter.interpret(tokens, false);
     }
 }
