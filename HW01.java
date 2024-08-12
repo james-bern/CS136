4291234d915261178a3f6096987a774c1d8fb684
Jim <jmb15@williams.edu>
2024-08-11 20:39:44 -0400

    asdf

--- HW01.java
+++
@@ -1,9 +0,0 @@
-class HW01 extends Cow {
-	public static void main(String[] arguments) {
-		System.out.println("Hello World");
-        while (begin_frame()) {
-            draw_set_color(0.0, 0.0, 1.0);
-            fill_circle(8.0, 4.5, 1.0);
-        }
-	}
-}
